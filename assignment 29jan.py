e958d19538f9457263a000aa771bfa5a5107696d
Bharat Sharma <117817162+bharatsharma07@users.noreply.github.com>
2023-08-01 20:12:34 +0530

    Add files via upload

---
+++ assignment 29jan.py
@@ -0,0 +1,129 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# # ASSIGNMENT 29 JAN
+
+# In[2]:
+
+
+# answer1
+# python was developed by guido van rossum in 80s and released in 1991.
+
+
+# In[3]:
+
+
+#answer 2
+# it supports oops ,including features like abstraction,inheritance  ,functional programming through features like lambda,map,
+# filter ,reduce functions.
+
+
+# In[4]:
+
+
+# answer 3
+#yes python is a case sensitive lang. when dealing with identifiers.here small 'a' and 'A' are different because haave different
+# ASCII values
+
+
+# In[5]:
+
+
+# answer 4
+#the correct extension of python file : ".py".
+
+
+# In[7]:
+
+
+#answer5
+# python code can be intepreted means each line of code executed by line by line.
+
+
+# In[8]:
+
+
+#answer6
+#when we used a few blocks of code used to define in python then it should be called as functions.
+
+
+# In[9]:
+
+
+# answer 7
+# (#)hash is used in python to give single line commments.
+
+
+# In[1]:
+
+
+# answer 8
+# import platform
+# print(platform.python_version()) 
+
+
+
+#import sys
+#sys.version 
+# by applying this it also gives the  current version.
+
+
+# In[2]:
+
+
+# answer 9
+# python supports the creation of anonymous functions at runtime,using a construct called "LAMBDA".
+
+
+# In[3]:
+
+
+# answer 10 
+#pip stands for pip install packages.it is a package management system used to install and manage software packages written in python.
+
+
+# In[4]:
+
+
+# answer 11
+# there are many built in functions in python.some are here:
+# 1) lambda 2)sort 3)remove 4) reverse   5) len 
+
+
+# In[5]:
+
+
+# answer 12
+# it is not defined the maximum length of an identifier(var,func,class).
+
+
+# In[6]:
+
+
+# ans 13
+# benefits of python:
+# 1) easy to learn 2)open-source and free   3)high demand in the job market  4)versatile  5)platform independent
+
+
+# In[7]:
+
+
+# ans 14
+# memory management in python is automatic handled by a garbage collector that periodically identifies and deletes objects that
+# are no long being used by the program.
+
+
+# In[8]:
+
+
+# ans 15
+# download latest verion of python from official website .onand ce python is installed,open cmd type python .and set the path in 
+# enviromental variables.and then set the path .
+
+
+# In[9]:
+
+
+# ans 16
+# yes indentation is required in python .if not maintain then it will show indentation error.
+
